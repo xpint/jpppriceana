5ed6c66b406dd89fd9113d7ba6f8b82f884c0565
xpint <xpint@qq.com>
2016-08-24 16:35:32 +0800

    add bS60 s60 bS20 s20 ana ...

--- hq分析.sql
+++ hq分析.sql
@@ -105,6 +105,39 @@ WHERE time >= '11:29:45'
 GROUP BY date
 
 
+SELECT 
+	date,
+	max(jsprice) AS 警示价,
+	min(time) AS 出价时间, 
+	min(hprice) AS 出价时间点最高价,
+	max(hprice)-300 AS 最终最低成交价, 
+	max(hprice) AS 最终最高成交价, 
+	max(hprice)-300-min(hprice) AS 出价时间段到最低成交价格增加档位,
+	max(hprice)-min(jsprice)-300 AS 成交价较警示价上涨幅度,
+	max(hprice)-300-min(hprice)+300 AS 出价时间点最低价基础自定义加价幅度,
+	max(hprice)-300-min(hprice)+300+300 AS 出价时间点最低价基础自定义加价幅度至最高价
+FROM "hq$" 
+WHERE time >= '11:29:40'
+GROUP BY date
+
+
+
+SELECT 
+	date,
+	max(jsprice) AS 警示价,
+	min(time) AS 出价时间, 
+	min(hprice) AS 出价时间点最高价,
+	max(hprice)-300 AS 最终最低成交价, 
+	max(hprice) AS 最终最高成交价, 
+	max(hprice)-300-min(hprice) AS 出价时间段到最低成交价格增加档位,
+	max(hprice)-min(jsprice)-300 AS 成交价较警示价上涨幅度,
+	max(hprice)-300-min(hprice)+300 AS 出价时间点最低价基础自定义加价幅度,
+	max(hprice)-300-min(hprice)+300+300 AS 出价时间点最低价基础自定义加价幅度至最高价
+FROM "hq$" 
+WHERE time >= '11:30:00'
+GROUP BY date
+
+
 
 
 SELECT 
